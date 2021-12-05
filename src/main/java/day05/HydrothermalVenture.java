88b4572436c23a58c058d87c6e2344f2ba67f5ec
Nils-Emil Lille <nilsemil.lille@gmail.com>
2021-12-05 12:56:46 +0200

    Day5 part 2

--- src/main/java/day05/HydrothermalVenture.java
+++ src/main/java/day05/HydrothermalVenture.java
@@ -24,25 +24,67 @@ public class HydrothermalVenture {
             int x2 = Integer.parseInt(cordinateTo[0]);
             int y1 = Integer.parseInt(cordinateFrom[1]);
             int y2 = Integer.parseInt(cordinateTo[1]);
-            if (isHorizontalOrVertical(x1, x2, y1, y2)) {
-                int xDiff = Math.max(x1, x2) - Math.min(x1, x2);
-                int yDiff = Math.max(y1, y2) - Math.min(y1, y2);
-                if (Objects.equals(y1, y2)) {
-                    System.out.println("Filling horizontal Line"  + line);
-                    int smallerX = Math.min(x1, x2);
-                    int smallerY = Math.min(y1, y2);
-                    fillHorizon(map, smallerX, smallerY, Math.abs(xDiff));
-                }
-                if (Objects.equals(x1, x2)) {
-                    System.out.println("Filling vertical Line" + line);
-                    int smallerX = Math.min(x1, x2);
-                    int smallerY = Math.min(y1, y2);
-                    fillVertical(map, smallerX, smallerY, Math.abs(yDiff));
-                }
+            int xDiff = Math.max(x1, x2) - Math.min(x1, x2);
+            int yDiff = Math.max(y1, y2) - Math.min(y1, y2);
+            int smallerX = Math.min(x1, x2);
+            int smallerY = Math.min(y1, y2);
+            if (Objects.equals(y1, y2)) {
+                fillHorizon(map, smallerX, smallerY, Math.abs(xDiff));
+            } else if (Objects.equals(x1, x2)) {
+                fillVertical(map, smallerX, smallerY, Math.abs(yDiff));
+            } else {
+                fillDiagonal(map, x1, x2, y1, y2, Math.abs(xDiff), Math.abs(yDiff), line);
             }
         }
-        System.out.println(map);
         int count = countHits(map);
+        System.out.println(count);
+        for (int i = 0; i < MAP_SIZE; i++) {
+            for (Integer[] arr : map) {
+                if (arr[i] == 0) {
+                    System.out.print("-");
+                } else {
+                    System.out.print(arr[i]);
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillDiagonal(Integer[][] map, int x1, int x2, int y1, int y2, int abs, int abs1, String line) {
+        if (x1 < x2 && y2 < y1) {
+            System.out.println("From bottom left to up right " + line);
+            fillDiagonalUpVol2(map, x1, y2, abs, line);
+        } else if (x1 < x2 && y2 > y1) {
+            System.out.println("From upper left to down right " + line);
+            fillDiagonalUp(map, x1, y1, abs, line);
+        } else if (x1 > x2 && y2 < y1) {
+            System.out.println("From bottom right to up left " + line);
+            fillDiagonalUp(map, x2, y2, abs, line);
+        } else if (x1 > x2 && y2 > y1) {
+            System.out.println("From upper right to down left " + line);
+            fillDiagonalUpVol2(map, x2, y1, abs, line);
+        }
+    }
+
+    private static void fillDiagonalUpVol2(Integer[][] map, int x, int y, int moveXTo, String line) {
+        for (int i = 0; i <= moveXTo; i++) {
+            int newX = x + i;
+            int newY = y + (moveXTo - i);
+            Integer initialCount = map[newX][newY];
+            map[newX][newY] = initialCount + 1;
+            System.out.println(newX + "," + newY + " marked to " + (initialCount + 1));
+        }
+    }
+
+    private static void fillDiagonalUp(Integer[][] map, int x, int y, int moveXTo, String line) {
+        for (int i = 0; i <= moveXTo; i++) {
+            int newX = x + i;
+            int newY = y + i;
+            Integer initialCount = map[newX][newY];
+            map[newX][newY] = initialCount + 1;
+            System.out.println(newX + "," + newY + " marked to " + (initialCount + 1));
+        }
     }
 
     private static int countHits(Integer[][] map) {
@@ -51,7 +93,7 @@ public class HydrothermalVenture {
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
                 if (map[i][j] >= 2) {
-                    count ++;
+                    count++;
                 }
             }
         }
@@ -63,9 +105,7 @@ public class HydrothermalVenture {
             int yCordinate = y + increment;
             Integer initialCount = map[x][yCordinate];
             map[x][yCordinate] = initialCount + 1;
-            System.out.println(x + "," + yCordinate+ " marked to "  + (initialCount + 1));
         }
-        System.out.println("----------");
     }
 
     private static void fillHorizon(Integer[][] map, Integer x, Integer y, Integer xDiff) {
@@ -73,14 +113,8 @@ public class HydrothermalVenture {
             int xCordinate = x + increment;
             Integer initialCount = map[xCordinate][y];
             map[xCordinate][y] = initialCount + 1;
-            System.out.println(xCordinate + "," + y+ " marked to "  + (initialCount + 1));
         }
-        System.out.println("----------");
 
-    }
-
-    private static boolean isHorizontalOrVertical(Integer x1, Integer x2, Integer y1, Integer y2) {
-        return Objects.equals(x1, x2) || Objects.equals(y1, y2);
     }
 
     private static Integer[][] initMap() {
