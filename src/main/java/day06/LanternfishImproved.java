dfe832fa7ec3a56c31965f39eec36628bcd22703
Nils-Emil Lille <nilsemil.lille@gmail.com>
2021-12-06 09:05:23 +0200

    Day6 bugfix for day 2

--- src/main/java/day06/LanternfishImproved.java
+++ src/main/java/day06/LanternfishImproved.java
@@ -16,37 +16,45 @@ public class LanternfishImproved {
     private static void solve() {
         List<String> csvLines = FileUtils.getCsvLines("day6.csv");
         List<Integer> fishes = Arrays.stream(csvLines.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        HashMap<Integer, Integer> map = initMap();
+        HashMap<Integer, Long> map = initMap();
         for (Integer fish : fishes) {
-            int initialValue = map.get(fish);
-            map.put(fish, initialValue + 1);
+            Long initialValue = map.get(fish);
+            map.put(fish, initialValue + 1L);
         }
-        int newFishOnNewDay = map.get(0) * 2;
-        for (int j = 0; j <= 82; j++) {
-            System.out.println("Day " + j);
+        Long newFishOnNewDay = 0L;
+        Long resettingFishes = 0L;
+        for (int j = 0; j < 256; j++) {
+            System.out.println("Day " + j + " number of fish " + countFish(map));
             for (int age = 0; age <= 8; age++) {
                 if (age == 0) {
-                    newFishOnNewDay = map.get(0) * 2;
+                    newFishOnNewDay = map.get(0);
+                    resettingFishes = map.get(0);
                     map.put(0, map.get(1));
                 } else if (age == 8) {
                     map.put(8, newFishOnNewDay);
+                } else if (age == 6) {
+                    map.put(age, map.get(age + 1) + resettingFishes);
                 } else {
                     map.put(age, map.get(age + 1));
                 }
             }
-
         }
-        int counter = 0;
-        for (int i = 0; i <= 8; i++) {
-            counter += map.get(i);
-        }
+        Long counter = countFish(map);
         System.out.println(counter);
     }
 
-    private static HashMap<Integer, Integer> initMap() {
-        HashMap<Integer, Integer> map = new HashMap();
+    private static Long countFish(HashMap<Integer, Long> map) {
+        Long counter = 0L;
         for (int i = 0; i <= 8; i++) {
-            map.put(i, 0);
+            counter += map.get(i);
+        }
+        return counter;
+    }
+
+    private static HashMap<Integer, Long> initMap() {
+        HashMap<Integer, Long> map = new HashMap();
+        for (int i = 0; i <= 8; i++) {
+            map.put(i, 0L);
         }
         return map;
     }
