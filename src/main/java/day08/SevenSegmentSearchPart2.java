01dd1faf25b23efc632c2cad4294041371ccfe3e
Nils-Emil Lille <nilsemil.lille@gmail.com>
2021-12-08 22:18:13 +0200

    AOC day 8 fix part 2 bugs

--- src/main/java/day08/SevenSegmentSearchPart2.java
+++ src/main/java/day08/SevenSegmentSearchPart2.java
@@ -13,26 +13,40 @@ public class SevenSegmentSearchPart2 {
     }
 
     private static void solve() {
-        List<String> csvLines = FileUtils.getCsvLines("day8_example.csv");
+        List<String> csvLines = FileUtils.getCsvLines("day8.csv");
         List<Integer> uniqueNumbers = new ArrayList();
 
         uniqueNumbers.add(2); // 1
         uniqueNumbers.add(3); // 7
         uniqueNumbers.add(4); // 4
         uniqueNumbers.add(7); // 8
-        Set<String> set = new HashSet<>();
         int counter = 0;
         for (String line : csvLines) {
             String[] split = line.split(" \\| ");
             HashMap<Integer, String> numbers = getNumbers(line);
+            HashMap<String, Integer> numbersReverse = reverese(numbers);
             String[] output = split[1].split(" ");
+            String rowResult = "";
             for (String number : output) {
-                set.add(number);
-                if (uniqueNumbers.contains(number.length())) {
-                    counter++;
-                }
+                char[] a = number.toCharArray();
+                sort(a);
+                String e = new String(a);
+                rowResult = rowResult + numbersReverse.get(e).toString();
             }
+            counter += Integer.parseInt(rowResult);
         }
+
+        System.out.println(counter);
+    }
+
+    private static HashMap<String, Integer> reverese(HashMap<Integer, String> numbers) {
+        HashMap<String, Integer> reversedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : numbers.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            reversedMap.put(value, key);
+        }
+        return reversedMap;
     }
 
 
@@ -44,17 +58,7 @@ public class SevenSegmentSearchPart2 {
         numbersBucket.put(6, new ArrayList<>()); // 0, 6, 9
         numbersBucket.put(5, new ArrayList<>()); // 2. 3, 5
         numbersBucket.put(7, new ArrayList<>()); // 8
-
-        HashMap<String, String> hashMap = new HashMap();
-        hashMap.put("a", null);
-        hashMap.put("b", null);
-        hashMap.put("c", null);
-        hashMap.put("d", null);
-        hashMap.put("e", null);
-        hashMap.put("f", null);
-        hashMap.put("g", null);
         String fullLine = line.replace(" | ", " ");
-
         List<String> numbers = Arrays.asList(fullLine.split(" "));
         for (String num : numbers) {
             List<String> list = numbersBucket.get(num.length());
@@ -65,12 +69,7 @@ public class SevenSegmentSearchPart2 {
                 list.add(e);
             }
         }
-
-        HashMap<Integer, String> realNumbers = new HashMap();
-        char[] one = numbersBucket.get(2).get(0).toCharArray();
-        char[] four = numbersBucket.get(4).get(0).toCharArray();
-        char[] seven = numbersBucket.get(7).get(0).toCharArray();
-        char[] eight = numbersBucket.get(3).get(0).toCharArray();
+        HashMap<Integer, String> realNumbers = new HashMap<>();
         realNumbers.put(1, numbersBucket.get(2).get(0));
         realNumbers.put(7, numbersBucket.get(3).get(0));
         realNumbers.put(4, numbersBucket.get(4).get(0));
@@ -81,7 +80,6 @@ public class SevenSegmentSearchPart2 {
             int i2 = mapToNumber(numbersBucket, numbersBucket.get(5).get(i));
             realNumbers.put(i2, numbersBucket.get(5).get(i));
         }
-        // TODO
         return realNumbers;
     }
 
@@ -94,26 +92,24 @@ public class SevenSegmentSearchPart2 {
         int interSectionsWith4 = getIntersection(four, num.toCharArray());
         int interSectionsWith7 = getIntersection(seven, num.toCharArray());
         int interSectionsWith8 = getIntersection(eight, num.toCharArray());
-//        numbersBucket.put(6, new ArrayList<>()); // 0, 6, 9
-//        numbersBucket.put(5, new ArrayList<>()); // 2. 3, 5
         if (interSectionsWith1 == 1 && interSectionsWith4 == 3 && interSectionsWith7 == 6 && interSectionsWith8 == 2) {
-            return 0;
-        }
-        if (interSectionsWith1 == 2 && interSectionsWith4 == 4 && interSectionsWith7 == 6 && interSectionsWith8 == 3) {
             return 6;
         }
-        if (interSectionsWith1 == 2 && interSectionsWith4 == 3 && interSectionsWith7 == 6 && interSectionsWith8 == 3) {
+        if (interSectionsWith1 == 2 && interSectionsWith4 == 4 && interSectionsWith7 == 6 && interSectionsWith8 == 3) {
             return 9;
+        }
+        if (interSectionsWith1 == 2 && interSectionsWith4 == 3 && interSectionsWith7 == 6 && interSectionsWith8 == 3) {
+            return 0;
         }
 
         if (interSectionsWith1 == 2 && interSectionsWith4 == 3 && interSectionsWith7 == 5 && interSectionsWith8 == 3) {
-            return 5;
+            return 3;
         }
         if (interSectionsWith1 == 1 && interSectionsWith4 == 3 && interSectionsWith7 == 5 && interSectionsWith8 == 2) {
-            return 2;
+            return 5;
         }
         if (interSectionsWith1 == 1 && interSectionsWith4 == 2 && interSectionsWith7 == 5 && interSectionsWith8 == 2) {
-            return 3;
+            return 2;
         }
         return 0;
     }
